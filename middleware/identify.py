9abc8718f5f6a9ba563d48130c06fab28cf5dc78
Martial Wafo <martialo218@gmail.com>
2024-11-27 17:10:41 +0100

    fix headers

--- middleware/identify.py
+++ middleware/identify.py
@@ -185,6 +185,7 @@ class SessionCookieValidator(IdentityValidator):
             bool: True if session validation succeeds, False otherwise.
         """
         headers = {"X-Requested-With": "XMLHttpRequest", **request.headers}
+        headers = {"authorization": headers.get("authorization"), "X-Requested-With": "XMLHttpRequest"}
         log.info(f"HEADERS: {headers}")
         if await self._attempt_session_validation(request, headers):
             return True
